876fcbccb4cdf2b4200916c499b3495f764bf9ab
mt-shke <michel.tcha@gmail.com>
2023-10-11 23:02:11 +0200

    favicon added

--- src/pages/graphsPage/GraphsPage3/GraphsPage3Header/index.tsx
+++ src/pages/graphsPage/GraphsPage3/GraphsPage3Header/index.tsx
@@ -10,7 +10,6 @@ const GraphsPage3Header: React.FC<ICtxProps> = ({ ctx }) => {
    if (updatedOptions) {
       content = <PieDataHeader updatedOptions={updatedOptions} />;
    }
-
    return (
       <div className="min-h-[50vh] w-full py-1 flex flex-1 md:min-h-0 md:row-start-1 md:row-end-2">
          <header className="flex flex-1 p-2 md:grid-cols-3 md:grid-rows-1 bg-main-grey4 overflow-hidden rounded">
