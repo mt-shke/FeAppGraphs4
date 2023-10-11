27c22e98f85a8f81245451012db4e19e292cdea6
mt-shke <michel.tcha@gmail.com>
2023-10-11 11:39:49 +0200

    pie top data

--- src/pages/graphsPage/GraphsPage3/GraphsPage3Section/PieDataReportTop.tsx
+++ src/pages/graphsPage/GraphsPage3/GraphsPage3Section/PieDataReportTop.tsx
@@ -1,27 +1,58 @@
 import { IUpdatedOptions } from ".";
 import { TransactionType } from "../../../../models/ts/types";
+import { dateToEnFormat } from "../../../../utils/dates";
 
 export interface ITransactionsProps {
    transactions: TransactionType[];
 }
 
 const PieDataReportTop: React.FC<IUpdatedOptions> = ({ transactions }) => {
-   console.log(transactions);
-
    const totalTransactions = transactions.length;
-   const amounts = transactions.map((item) => item.amount);
+   const amounts = transactions.map((item) => item.amount).sort();
    const totalAmount = amounts.reduce((prev, curr) => prev + curr, 0);
 
-   console.log("amounts :", amounts);
+   let highest,
+      lowest,
+      firstTransaction,
+      firstTransactionDate,
+      lastTransaction,
+      lastTransactionDate = "";
+
+   if (totalTransactions >= 1) {
+      lowest = amounts[0];
+      highest = amounts[amounts.length - 1];
+      firstTransaction = transactions[0];
+      firstTransactionDate = dateToEnFormat(firstTransaction.date);
+      lastTransaction = transactions[transactions.length - 1];
+      lastTransactionDate = dateToEnFormat(lastTransaction.date);
+   }
 
    return (
-      <article>
+      <article className="p-2">
          <ul>
             <li>
-               {totalTransactions} {`transactions`}
+               <h2 className="font-bold">
+                  {totalTransactions} {`transactions`}
+               </h2>
             </li>
             <li>
                {`Total amount: `}${totalAmount}
+            </li>
+            <li>
+               {`First transaction: `}
+               {firstTransactionDate}
+            </li>
+            <li>
+               {`Last transaction: `}
+               {lastTransactionDate}
+            </li>
+            <li>
+               {`Lowest transaction: `}
+               {lowest}
+            </li>
+            <li>
+               {`Highest transaction: `}
+               {highest}
             </li>
          </ul>
       </article>
