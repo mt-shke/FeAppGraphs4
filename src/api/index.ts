ddec73e3b26db42b1e8828079149ed85b27d8d06
mt-shke <michel.tcha@gmail.com>
2023-10-13 15:41:12 +0200

    api update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios from "redaxios";
 // export const API_URL = "https://product-feedback-api-vspp.onrender.com/api/v1";
-export const API_URL = "http://localhost:5001/api/v1";
+// export const API_URL = "http://localhost:5001/api/v1";
+export const API_URL = "http://graphs-backend-render.onrender.com/api/v1";
 
 export const authAxios = axios.create({
    baseURL: API_URL,
